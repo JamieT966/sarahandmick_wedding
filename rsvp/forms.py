e3d465b1e4a766fac7a21083336d1b5e82f8177f
Jamie <jamietarpey1@gmail.com>
2023-10-09 18:32:45 +0100

    reworked form

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import RSVP, DietaryRequirement
 
 class RSVPForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RSVPForm, self).__init__(*args, **kwargs)
+        self.fields['other_dietary_input'].label = ""
+
     YES_NO_CHOICES = [
         (True, 'Yes'),
         (False, 'No')
@@ -14,30 +18,35 @@ class RSVPForm(forms.ModelForm):
     ]
 
     will_attend = forms.ChoiceField(
+        label="Are you attending?",
         choices=YES_NO_CHOICES,
         widget=forms.RadioSelect,
         required=True
     )
     both_attending = forms.ChoiceField(
+        label="Is person two attending (if applicable)?",
         choices=YES_NO_CHOICES,
         widget=forms.RadioSelect,
         required=False
     )
     dietary_requirements = forms.ModelMultipleChoiceField(
+        label = "Do you have any other dietary requirements?",
         queryset=DietaryRequirement.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
     other_dietary_input = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Other Dietary Requirements'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Other Dietary Requirements', 'class': 'no-label'}),
         required=False
     )
     attending_day2 = forms.ChoiceField(
+        label="Will you be attending day two?",
         choices=DAY2_CHOICES,
         widget=forms.RadioSelect,
         required=False
     )
     music_requests = forms.CharField(
+        label="Is there any music you want to hear on the night?",
         widget=forms.TextInput(),
         required=False
     )
@@ -45,6 +54,16 @@ class RSVPForm(forms.ModelForm):
     class Meta:
         model = RSVP
         fields = ['name', 'name2', 'will_attend', 'both_attending', 'dietary_requirements', 'other_dietary_input', 'attending_day2', 'music_requests']
+        labels = {
+            'name': 'Name on Invite',
+            'name2': 'Name Two on Invite (If Applicable)',
+            'will_attend': 'Are you attending?',
+            'both_attending': 'Is person two attending (if applicable)?',
+            'dietary_requirements': 'Do you have any dietary requirements?',
+            'other_dietary_input': 'Do you have any other dietary requirements?',
+            'attending_day2': 'Will you be attending day two?',
+            'music_requests': 'Is there any music you want to hear on the night?',
+        }
 
     def clean(self):
         cleaned_data = super().clean()
