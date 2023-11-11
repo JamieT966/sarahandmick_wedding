d4e72ba4a76327b4c968cb64e23b051b02f834c7
Jamie <jamietarpey1@gmail.com>
2023-11-11 17:36:40 +0000

    changed day 2 venue

--- wedding_new/settings.py
+++ wedding_new/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$!u1%iw-56l#ka&&-00d=xe&erk2o1lqukkip&*v8hp%m^6ao3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1','jandegethitched','www.jandegethitched.com']
 
