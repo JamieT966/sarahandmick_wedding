a7288e16a7d788dc3e53bf8b85c38716f919eaf1
Jamie <jamietarpey1@gmail.com>
2024-07-23 16:50:10 +0100

    updated allowed users

--- wedding_new/settings.py
+++ wedding_new/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1','jandegethitched','www.jandegethitched.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
